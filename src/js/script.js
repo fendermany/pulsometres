48289de1b0aff4016e02e82167d273bfe1e96ff0
Dmitry <fendermany@gmail.com>
2020-12-15 21:38:00 +0200

    Form validation

--- src/js/script.js
+++ src/js/script.js
@@ -67,4 +67,36 @@ $(document).ready(function(){
 			$('.overlay, #order').fadeIn('slow');
 		})
 	});
+
+		function valideForms(form){
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				  },
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+	
+			messages: {
+				name: {
+					required: "Пожалуйста, введите своё имя",
+					minlength: jQuery.validator.format("Минимум {0} символа!")
+				  },
+				phone: "Пожалуйста, введите свой номер телефона",
+				email: {
+				  required: "Пожалуйста, введите свою почту",
+				  email: "Неправильно введён адрес почты"
+				}
+			  }
+		});
+	};
+
+	valideForms('#consultation-form');
+	valideForms('#consultation form');
+	valideForms('#order form');
 });
